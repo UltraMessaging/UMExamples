8cd4515800abf513ca56b375497966077fd6887b
Steve Glass <sglass@buster.(none)>
2015-06-24 16:45:38 -0400

    completed c sample for request_response

--- request_response/c/request_response.c
+++ request_response/c/request_response.c
@@ -1,4 +1,4 @@
-/* Code Disclaimer? */
+/* request_response.c - http://ultramessaging.github.io/UMExamples */
 
 #include <stdio.h>
 
@@ -24,8 +24,10 @@
   	}  \
 } while (0)
 
+/* Global var used to indicate that the application can exit after response received */
 int run = 1;
 
+/* Callback used to handle request message for receiver */
 int rcv_handle_msg(lbm_rcv_t *rcv, lbm_msg_t *msg, void *clientd)
 {
 	int err;
@@ -39,6 +41,7 @@ int rcv_handle_msg(lbm_rcv_t *rcv, lbm_msg_t *msg, void *clientd)
 	}
 }
 
+/* Callback used to handle response message */
 int handle_response(lbm_request_t *req, lbm_msg_t *msg, void *clientd)
 {
         switch (msg->type) {
@@ -52,15 +55,15 @@ int handle_response(lbm_request_t *req, lbm_msg_t *msg, void *clientd)
 main()
 {
 	lbm_context_t *ctx;                     /* Context object */
-        lbm_topic_t *stopic;                    /* Source Topic object */
-        lbm_src_t *src;                         /* Source object */
-        lbm_src_topic_attr_t * tattr;           /* Source topic attribute object */
-        lbm_context_attr_t * cattr;             /* Context attribute object */
-	lbm_rcv_t *rcv;      			/* Receive object: for subscribing to messages. */
+	lbm_topic_t *stopic;                    /* Source Topic object */
+	lbm_src_t *src;                         /* Source object */
+	lbm_src_topic_attr_t * tattr;           /* Source topic attribute object */
+	lbm_context_attr_t * cattr;             /* Context attribute object */
+	lbm_rcv_t *rcv;                         /* Receive object: for subscribing to messages. */
 	lbm_topic_t *rtopic;                    /* Receiver Topic object */
-	lbm_request_t *req;			/* Request object */
-        int err;                                /* Used for checking API return codes */
-  
+	lbm_request_t *req;                     /* Request object */
+	int err;                                /* Used for checking API return codes */
+
 #if defined(_WIN32)
         /* windows-specific code */
         WSADATA wsadata;
@@ -72,12 +75,13 @@ main()
         }
 #endif
   
-        err = lbm_context_attr_create(&cattr);
+	/* Initialize context atrributes and create context */
+	err = lbm_context_attr_create(&cattr);
 	EX_LBM_CHK(err);
   
-        err = lbm_context_create(&ctx, cattr, NULL, NULL);
+	err = lbm_context_create(&ctx, cattr, NULL, NULL);
 	EX_LBM_CHK(err);
-  
+
 	/* Create receiver for receiving request and sending response */
 	err = lbm_rcv_topic_lookup(&rtopic, ctx, "test.topic", NULL);
 	EX_LBM_CHK(err);
@@ -89,13 +93,13 @@ main()
 	err = lbm_src_topic_attr_create(&tattr);
 	EX_LBM_CHK(err);
   
-        err = lbm_src_topic_alloc(&stopic, ctx, "test.topic", tattr);
+	err = lbm_src_topic_alloc(&stopic, ctx, "test.topic", tattr);
   	EX_LBM_CHK(err);
 
-      	err = lbm_src_topic_attr_delete(tattr);
+	err = lbm_src_topic_attr_delete(tattr);
   	EX_LBM_CHK(err);
 
-        err = lbm_src_create(&src, ctx, stopic, NULL, NULL, NULL);
+	err = lbm_src_create(&src, ctx, stopic, NULL, NULL, NULL);
 	EX_LBM_CHK(err);
 
 	err = lbm_send_request(&req, src, "request", 7, handle_response, NULL, NULL, 0);
