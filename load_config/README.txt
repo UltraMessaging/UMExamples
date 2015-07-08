a9f38f706a5c52ac183261934dc924adcb7f6806
steve glass <steve.m.glass@gmail.com>
2015-07-07 21:19:42 -0400

    initial checkin for load_config samples

---
+++ load_config/README.txt
@@ -0,0 +1,3 @@
+Summary=A sample application that demonstrates how to load a configuration file for UM attributes
+
+Description=
---
+++ load_config/c/load_config.c
@@ -0,0 +1,38 @@
+/* load_config.c - http://ultramessaging.github.io/UMExamples */
+
+#include <stdio.h>
+
+#if defined(_MSC_VER)
+/* Windows-only includes */
+#include <winsock2.h>
+#else
+/* Unix-only includes */
+#include <stdlib.h>
+#include <unistd.h>
+#endif
+
+#include <lbm/lbm.h>
+
+/* Example error checking macro.  Include after each UM call. */
+#define EX_LBM_CHK(err) do { \
+        if ((err) < 0) { \
+                fprintf(stderr, "%s:%d, lbm error: '%s'\n", \
+                __FILE__, __LINE__, lbm_errmsg()); \
+                exit(1); \
+        }  \
+} while (0)
+
+#define LBM_CONFIG_FLAT_FILE "../flat_config.cfg"
+#define LBM_CONFIG_XML_FILE  "../xml_config.xml"
+#define LBM_APPLICATION_NAME "processA"
+
+main()
+{
+	int err;				/* Used for checking API return codes */
+
+	err = lbm_config((const char *)LBM_CONFIG_FLAT_FILE);
+	EX_LBM_CHK(err);
+
+	err = lbm_config_xml_file((const char *)LBM_CONFIG_XML_FILE, (const char *)LBM_APPLICATION_NAME);
+	EX_LBM_CHK(err);
+}
